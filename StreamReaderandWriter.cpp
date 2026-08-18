e3decebdfa9969c025bf00edb972efe0f0e67f6a
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 10:46:39 +0700

    menambahkan outfile dan infile

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -26,4 +26,18 @@ int main()
         //menulis dan memasukkan nilai dari variabel baris ke dalam file
         outfile << baris << endl;
     }
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        
+    }
 }
