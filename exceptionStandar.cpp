5c069985309b81d2698383194a841eca64bbb3a5
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 10:10:19 +0700

    memberi deklarasi array

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -6,4 +6,11 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panajng index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke 5 yang berarti akan terjadi error
+        cout << data.at(3) << endl;
+    }
 }
