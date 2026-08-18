0544a7615470b71e318abf3818f5b69006789e24
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 10:25:26 +0700

    menambahkan outfile & ifstream

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -30,4 +30,14 @@ int main()
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan Membaca File" << endl;
 }
