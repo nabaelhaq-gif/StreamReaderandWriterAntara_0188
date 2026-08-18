b8d6c66d02a9d91f3128ec13f3939d9deb71ec51
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 10:19:33 +0700

    menunjuk ke file

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -11,4 +11,11 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open("D:\\PemdasAntara\\coba\\" + NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 }
