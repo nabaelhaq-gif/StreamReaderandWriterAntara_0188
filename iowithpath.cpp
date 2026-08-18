4c7c120918855ffe519e31c2334d7f6abc327336
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-08-18 10:15:48 +0700

    menambahkan deklarasi variabel dan input menangkap nama file

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -2,3 +2,13 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string bairs, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+}
